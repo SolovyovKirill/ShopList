623771e75c00d6fff910d01442dc8671f922face
SolovyovKirill <Solowyow.kirya@gmail.com>
2021-11-24 18:52:16 +0300

    data update with Mappers and MediatorLivedata

---
+++ app/src/main/java/com/shoppinglist/data/ShopListMapper.kt
@@ -0,0 +1,24 @@
+package com.shoppinglist.data
+
+import com.shoppinglist.domain.shopitem.ShopItem
+
+class ShopListMapper {
+
+    fun mapEntityToDbModel(shopItem: ShopItem) = ShopItemDbModel(
+        id = shopItem.id,
+        name = shopItem.name,
+        count = shopItem.count,
+        enable = shopItem.enable
+    )
+
+    fun mapDbModelToEntity(shopItemDbModel: ShopItemDbModel) = ShopItem(
+        id = shopItemDbModel.id,
+        name = shopItemDbModel.name,
+        count = shopItemDbModel.count,
+        enable = shopItemDbModel.enable
+    )
+
+    fun mapListDbModelToEntity(list: List<ShopItemDbModel>) = list.map {
+        mapDbModelToEntity(it)
+    }
+}
