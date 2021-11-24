241579a3524b233b0a91fc9bfe08ccc2ebf30757
SolovyovKirill <Solowyow.kirya@gmail.com>
2021-11-24 23:33:35 +0300

    application with viewModelScope

--- app/src/main/java/com/shoppinglist/domain/usecase/addshopitemusecase/AddShopItemUseCase.kt
+++ app/src/main/java/com/shoppinglist/domain/usecase/addshopitemusecase/AddShopItemUseCase.kt
@@ -5,7 +5,7 @@ import com.shoppinglist.domain.shopitem.ShopItem
 
 class AddShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun addShopItem(shopItem: ShopItem) {
+    suspend fun addShopItem(shopItem: ShopItem) {
         shopListRepository.addShopItem(shopItem)
     }
 }
--- app/src/main/java/com/shoppinglist/domain/usecase/deleteshopitemusecase/DeleteShopItemUseCase.kt
+++ app/src/main/java/com/shoppinglist/domain/usecase/deleteshopitemusecase/DeleteShopItemUseCase.kt
@@ -5,7 +5,7 @@ import com.shoppinglist.domain.shopitem.ShopItem
 
 class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun deleteShopItem(shopItem: ShopItem) {
+    suspend fun deleteShopItem(shopItem: ShopItem) {
         shopListRepository.deleteShopItem(shopItem)
     }
 }
